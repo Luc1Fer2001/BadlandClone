efedb56edbed01583d948cd806f01980649ba893
Luc1fer2001 <trankvilizator008@gmail.com>
2020-12-07 14:45:56 +0200

    new

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public SpriteRenderer[] spriteRenderer;
+    [SerializeField]
+    private float verticalSpeed = 2;
+    private Rigidbody2D rigidBody2D;
+    public void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, verticalSpeed);
+        }
+        else
+        {
+            rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, -verticalSpeed);
+        }
+
+        if(transform.position.x <= -12 || transform.position.y < -6 || transform.position.y > 6)
+        {
+            Destroy(gameObject);
+            
+        }
+    }
+
+
+}
